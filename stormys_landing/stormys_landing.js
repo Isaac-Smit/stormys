d8641b55e1160b8bc441e4830454076e095b7ee1
Isaac Smith <160376331+Isaac-Smit@users.noreply.github.com>
2024-08-08 18:17:22 +1200

    Next section completed

--- stormys_landing/stormys_landing.js
+++ stormys_landing/stormys_landing.js
@@ -1,21 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-    gsap.registerPlugin(ScrollTrigger);
-
-    ScrollTrigger.create({
-        animation: gsap.to(".logo", {
-            y: -180, // Adjust this value as needed
-            scale: 0.38, // Scale to the desired final size
-            yPercent: -50,
-            ease: "none"
-        }),
-        scrub: true,
-        trigger: ".content-container",
-        start: "top bottom",
-        end: "top center",
-        pin: ".logo-container",
-        pinSpacing: false
-    });
-});
+// ScrollTrigger.create({
+//     animation:gsap.from(".logo", {
+//       y:"100vh",
+//       scale:4,
+//       yPercent:-50
+//     }),
+//     scrub:true,
+//     trigger: ".spacer",
+//     start: "top bottom",
+//     endTrigger: '.spacer',
+//     end: 'top center',
+//     markers: true,
+//     pin: true,
+//     pinSpacing: false
+//   })
 
 
 
