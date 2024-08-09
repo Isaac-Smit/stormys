1d4e52f9e1bed48ee83bb91e2cdb8fe9c0e07523
Isaac Smith <160376331+Isaac-Smit@users.noreply.github.com>
2024-08-09 18:56:36 +1200

    Review carousel done

--- stormys_landing/stormys_landing.js
+++ stormys_landing/stormys_landing.js
@@ -16,4 +16,26 @@
 
 
 
+let currentIndex = 0;
+const animationSpeed = 10000;
+
+function moveClick (direction) {
+    clearInterval(coursel);
+    moveCarousel(direction);
+    setTimeout(() => { coursel = setInterval(() => { moveCarousel(1);}, animationSpeed)}, 30000);
+}
+
+function moveCarousel(direction) {
+    const items = document.querySelectorAll('.carousel-item');
+    items[currentIndex].classList.remove('active');
+    
+    currentIndex = (currentIndex + direction + items.length) % items.length;
+    
+    items[currentIndex].classList.add('active');
+}
+
+// Auto-slide functionality
+let coursel = setInterval(() => {
+    moveCarousel(1);
+}, animationSpeed);
 
